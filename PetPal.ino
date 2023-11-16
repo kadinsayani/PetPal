f11119b8c1e317e87ce2c67d92712491eec467f6
Kadin Sayani <me@kadinsayani.dev>
2023-11-15 22:36:39 -0700

    bug fixes

--- PetPal.ino
+++ PetPal.ino
@@ -1,23 +1,21 @@
 #include <IRremote.h>
 #include <Keypad.h>
-#include <LiquidCrystal.h>
 #include <RTClib.h>
 #include <Servo.h>
 
-const int OK_BUTTON_IR_CODE = 0xFD708F;
-const int RECV_PIN = 7;
-IRrecv irrecv(RECV_PIN);
-decode_results results;
+const uint32_t OK_BUTTON_IR_CODE = 0xF20DFF00;
+const int IR_RECEIVE_PIN = 11;
 
-const int motorPin = 10;
+const int motorPin = 9;
 Servo motor;
 
 RTC_DS1307 rtc;
+DateTime now;
+DateTime targetTime;
 
 void setup() {
   Serial.begin(9600);
-  irrecv.enableIRIn();
-  irrecv.blink13(true);
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
   motor.attach(motorPin);
 
   if (!rtc.begin()) {
@@ -25,90 +23,63 @@ void setup() {
     Serial.flush();
     abort();
   }
-  rtc.adjust(DateTime(2023, 11, 14, 8, 16, 0));
+
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  now = rtc.now();
+  targetTime = now + TimeSpan(0, 1, 0, 0);
+  Serial.println(now.unixtime());
+  Serial.println(targetTime.unixtime());
 }
 
 void loop() {
-  if (irrecv.decode(&results)) {
-    Serial.println(results.value, HEX);
+  now = rtc.now();
+  if (now.unixtime() >= targetTime.unixtime()) {
+    Serial.println("Motor triggered!");
+    motor.write(90);
+    delay(5000);
+    motor.write(0);
+  }
 
-    switch (results.value) {
-    case 0xFD708F:
+  if (IrReceiver.decode()) {
+    Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+    if (IrReceiver.decodedIRData.decodedRawData == OK_BUTTON_IR_CODE) {
+      Serial.println("OK button pressed!");
       setMotorTime();
-      break;
-    default:
-      break;
     }
-
-    irrecv.resume();
+    IrReceiver.resume();
   }
 }
 
 void setMotorTime() {
-  Serial.println("Enter the desired time using the remote (HH:MM format):");
-  String timeString = "";
-  while (!irrecv.decode(&results)) {
-    // Wait for IR input
+  Serial.println("Setting motor time...");
+  if (IrReceiver.decode()) {
+    char buttonChar = getButtonChar(IrReceiver.decodedIRData.decodedRawData);
+    Serial.println(buttonChar);
+    IrReceiver.resume();
   }
-
-  while (results.value != OK_BUTTON_IR_CODE) {
-    if (irrecv.decode(&results)) {
-      char buttonChar = getButtonChar(results.value);
-      if (buttonChar != '\0') {
-        char buttonChar = getButtonChar(results.value);
-        timeString += buttonChar;
-        Serial.print(buttonChar);
-      }
-      irrecv.resume();
-    }
-  }
-
-  int hours = timeString.substring(0, 2).toInt();
-  int minutes = timeString.substring(3, 5).toInt();
-
-  Serial.print("Setting motor to run at ");
-  Serial.print(hours);
-  Serial.print(":");
-  Serial.println(minutes);
-
-  setMotorStartTime(hours, minutes);
 }
 
-char getButtonChar(unsigned long irCode) {
+char getButtonChar(uint32_t irCode) {
   switch (irCode) {
-  case 0xFD08F7:
+  case 0xF30CFF00:
     return '1';
-  case 0xFD8877:
+  case 0xE718FF00:
     return '2';
-  case 0xFD48B7:
+  case 0xA15EFF00:
     return '3';
-  case 0xFD28D7:
+  case 0xF708FF00:
     return '4';
-  case 0xFDA857:
+  case 0xE31CFF00:
     return '5';
-  case 0xFD6897:
+  case 0xA55AFF00:
     return '6';
-  case 0xFD18E7:
+  case 0xBD42FF00:
     return '7';
-  case 0xFD9867:
+  case 0xAD52FF00:
     return '8';
-  case 0xFD58A7:
+  case 0xB54AFF00:
     return '9';
   default:
     return '\0';
-  }
-}
-
-void setMotorStartTime(int hours, int minutes) {
-  while (true) {
-    DateTime now = rtc.now();
-    if (now.hour() == hours && now.minute() == minutes) {
-      Serial.println("Motor triggered!");
-      motor.write(90);
-      delay(1000);
-      motor.write(0);
-      break;
-    }
-    delay(1000);
   }
 }
